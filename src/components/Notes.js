dd606a25a4ded5047f2e5a159752e74c6d2659b6
Alexander Grischuk <alexgrischuk@gmail.com>
2019-12-17 22:52:47 +0100

    Display button to remove the note

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 export const Notes = ({ notes }) => {
   return (
-    <ul className="list-group">
+    <ul className='list-group'>
       {notes.map(note => (
-        <li key={note.id} className="list-group-item">
-          {note.title}
+        <li key={note.id} className='list-group-item note'>
+          <strong>{note.title}</strong>
+
+          <button type='button' className='btn btn-outline-danger btn-sm'>
+            &times;
+          </button>
         </li>
       ))}
     </ul>
