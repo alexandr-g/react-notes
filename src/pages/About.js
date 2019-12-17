23b78c49a479214023091d43c213536ff5850e26
Alexander Grischuk <alexgrischuk@gmail.com>
2019-12-17 22:45:25 +0100

    Correct class names

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 export const About = () => (
-  <div class="jumbotron">
-    <div class="container">
-      <h1 class="display-4">React Notes</h1>
-      <p class="lead">
+  <div className='jumbotron'>
+    <div className='container'>
+      <h1 className='display-4'>React Notes</h1>
+      <p className='lead'>
         Version number <strong>xx.xx.xx</strong>
       </p>
     </div>
