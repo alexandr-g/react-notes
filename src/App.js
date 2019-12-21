57ba9c477c0a8f7039260b33a549cb4bcf3598a6
Alexander Grischuk <alexgrischuk@gmail.com>
2019-12-21 20:31:52 +0100

    Wrap the app in alert state

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ import { About } from './pages/About'
 
 import { Navbar, Alert } from './components'
 
+import { AlertState } from './context/alert/AlertState'
+
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className='container pt-4'>
-        <Alert />
-        <Switch>
-          <Route path={'/'} exact component={Home} />
-          <Route path={'/about'} component={About} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <AlertState>
+      <BrowserRouter>
+        <Navbar />
+        <div className='container pt-4'>
+          <Alert />
+          <Switch>
+            <Route path={'/'} exact component={Home} />
+            <Route path={'/about'} component={About} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </AlertState>
   )
 }
 
