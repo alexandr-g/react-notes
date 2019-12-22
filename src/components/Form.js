682da9f43cc90a969caa144f805fd111c0ed72a6
Alexander Grischuk <alexgrischuk@gmail.com>
2019-12-22 20:16:01 +0100

    Show alert on success

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { AlertContext } from '../context/alert/alertContext'
 
 export const Form = () => {
+  const [value, setValue] = useState('')
+  const alert = useContext(AlertContext)
+
+  const submitHandler = event => {
+    event.preventDefault()
+
+    alert.show(value, 'success')
+  }
+
   return (
-    <form>
-      <div className="form-group">
+    <form onSubmit={submitHandler}>
+      <div className='form-group'>
         <input
-          type="text"
-          className="form-control"
-          placeholder="Enter a note title"
+          type='text'
+          className='form-control'
+          placeholder='Enter a note title'
+          value={value}
+          onChange={e => setValue(e.target.value)}
         ></input>
       </div>
     </form>
