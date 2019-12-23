9ea49d13dda71e8ff4667688100a039b72cbc17a
Alexander Grischuk <alexgrischuk@gmail.com>
2019-12-23 23:18:06 +0100

    Support onRemove from parent component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import { FirebaseContext } from '../context/firebase/firebaseContext'
 import { Loader } from '../components/Loader'
 
 export const Home = () => {
-  const { loading, notes, fetchNotes } = useContext(FirebaseContext)
+  const { loading, notes, fetchNotes, removeNote } = useContext(FirebaseContext)
 
   useEffect(() => {
     fetchNotes()
@@ -17,7 +17,7 @@ export const Home = () => {
 
       <hr />
 
-      {loading ? <Loader /> : <Notes notes={notes} />}
+      {loading ? <Loader /> : <Notes notes={notes} onRemove={removeNote} />}
     </Fragment>
   )
 }
