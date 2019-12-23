5fdfaf887a606733112fd9cfd94ac729aa3e58bf
Alexander Grischuk <alexgrischuk@gmail.com>
2019-12-23 23:17:47 +0100

    Remove note on remove click

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,20 +1,22 @@
 import React from 'react'
 
-export const Notes = ({ notes }) => {
-  return (
-    <ul className='list-group'>
-      {notes.map(note => (
-        <li key={note.id} className='list-group-item note'>
-          <div>
-            <strong>{note.title}</strong>
-            <small>{note.date}</small>
-          </div>
+export const Notes = ({ notes, onRemove }) => (
+  <ul className='list-group'>
+    {notes.map(note => (
+      <li key={note.id} className='list-group-item note'>
+        <div>
+          <strong>{note.title}</strong>
+          <small>{note.date}</small>
+        </div>
 
-          <button type='button' className='btn btn-outline-danger btn-sm'>
-            &times;
-          </button>
-        </li>
-      ))}
-    </ul>
-  )
-}
+        <button
+          type='button'
+          onClick={() => onRemove(note.id)}
+          className='btn btn-outline-danger btn-sm'
+        >
+          &times;
+        </button>
+      </li>
+    ))}
+  </ul>
+)
