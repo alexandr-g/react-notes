ff5959a2e6a655d52a287e7b81bfc1cfdef224bd
Alexander Grischuk <alexgrischuk@gmail.com>
2019-12-23 22:45:17 +0100

    Add simple loader

---
+++ src/components/Loader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Loader = () => (
+  <div className='text-center'>
+    <div className='spinner-grow'>
+      <span className='sr-only'>Loading...</span>
+    </div>
+  </div>
+)
