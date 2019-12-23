d4663243f459de2425cdd0a3b404d10033dbfbfb
Alexander Grischuk <alexgrischuk@gmail.com>
2019-12-23 22:35:12 +0100

    Pull loading, notes, fetchNotes from context

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,9 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useContext, useEffect } from 'react'
 import { Form, Notes } from '../components'
+import { FirebaseContext } from '../context/firebase/firebaseContext'
 
 export const Home = () => {
-  const notes = new Array(3)
-    .fill('')
-    .map((_, i) => ({ id: i, title: `Note ${i + 1}` }))
+  const { loading, notes, fetchNotes } = useContext(FirebaseContext)
 
   return (
     <Fragment>
