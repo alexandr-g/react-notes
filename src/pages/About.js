57b0c66275689a072746a2539169678c0f6ff4cd
Alexander Grischuk <alexgrischuk@gmail.com>
2019-12-24 11:04:21 +0100

    Add description and app overview

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,8 +5,22 @@ export const About = () => (
     <div className='container'>
       <h1 className='display-4'>React Notes</h1>
       <p className='lead'>
-        Version number <strong>xx.xx.xx</strong>
+        Version number <strong>00.00.01</strong>
       </p>
+      <p>
+        {' '}
+        Ii is a simple <strong>React</strong> app made exclusively on &nbsp;
+        <strong>React Hooks</strong> with connection to &nbsp;
+        <strong>Firebase</strong>.
+      </p>
+      What you can do:
+      <ul>
+        <li>add a new note</li>
+        <li>remove note</li>
+        <li>fetch all notes from the server</li>
+        <li>react-route: navigate between pages</li>
+        <li>see alerts and close them</li>
+      </ul>
     </div>
   </div>
 )
